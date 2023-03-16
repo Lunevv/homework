b1df76fe85c95619ed64a77693aa236b92796c1b
Lunevv <87904083+Lunevv@users.noreply.github.com>
2023-03-17 00:08:43 +0100

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -5,12 +5,11 @@ url_weather = "https://wttr.in/{}"
 paylad = {"nTqM": "", "lang": "ru"}
 
 def get_weather():
-
     for location in weather_location:
         url = url_weather.format(location)
         response = requests.get(url, params=paylad)
         response.raise_for_status()
         print(response.text)
-
+    return
 if __name__ == '__main__':
     get_weather()
